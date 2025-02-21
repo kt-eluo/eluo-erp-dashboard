7623c9499a298b14a729a7db0029b1d5aaeaf657
kt-eluo <kt.eluo@eluocnc.com>
2025-02-21 11:09:13 +0900

    250221

--- app/(dashboard)/projects/page.tsx
+++ app/(dashboard)/projects/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from 'react';
 
 interface Project {
@@ -5,18 +7,26 @@ interface Project {
   [key: string]: any;
 }
 
-const [projects, setProjects] = useState<Project[]>([]);
+export default function ProjectsPage() {
+  const [projects, setProjects] = useState<Project[]>([]);
 
-const handleProjectSubmit = (projectData: any) => {
-  if (projectData._action === 'update') {
-    // 기존 프로젝트 목록에서 수정된 프로젝트를 찾아 업데이트
-    setProjects(prevProjects => 
-      prevProjects.map((project: Project) => 
-        project.id === projectData.id ? projectData : project
-      )
-    );
-  } else {
-    // 새 프로젝트 추가
-    setProjects(prevProjects => [...prevProjects, projectData]);
-  }
-}; 
+  const handleProjectSubmit = (projectData: any) => {
+    if (projectData._action === 'update') {
+      // 기존 프로젝트 목록에서 수정된 프로젝트를 찾아 업데이트
+      setProjects(prevProjects => 
+        prevProjects.map((project: Project) => 
+          project.id === projectData.id ? projectData : project
+        )
+      );
+    } else {
+      // 새 프로젝트 추가
+      setProjects(prevProjects => [...prevProjects, projectData]);
+    }
+  };
+
+  // ... 나머지 코드
+
+  return (
+    // ... JSX
+  );
+} 
